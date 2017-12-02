4daaef14ff57473ccb3a60ffa9a071372d85683d
D Kim <d.dkim026@gmail.com>
2017-12-02 06:50:13 -0500

    changes to the script
    aaa

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
 function generate ()
 {
+	document.getElementById("test").innerHTML="";
 	
 	var container = document.createElement("div");
 	container.setAttribute("class", "container"); //Have to append each of the following 
 
-/*	var problem = fields[k].problem;
-	var description = fields[k].description;*/
-
 	var row = document.createElement("div");
 	row.setAttribute("class", "row");
-	
-	var colm = document.createElement("div");
-	colm.setAttribute("class", "col-sm");
-	
-	
-	var labela = document.createElement("a");
-	labela.setAttribute("onmouseover", problem.toString()+"()");
 
-	labela.setAttribute("onmouseout", problem.toString()+"()");
+		for (var x =0; x< 3; x++)
+	{
+		var problem = "problemname"; //have to fix this
+		var description = "problem description";
+				
+		var scripts = document.createElement("script");
+		scripts.text = "function " +problem.toString()+"toggle (){" +"$('#" + problem.toString() +"').collapse('toggle');}";
+		document.head.appendChild(scripts);
+
+		
+		
+		var colm = document.createElement("div");
+		colm.setAttribute("class", "col-sm");
+		
+		
+		var labela = document.createElement("a");
+		labela.setAttribute("onmouseover", problem.toString()+"toggle()");
+
+		labela.setAttribute("onmouseout", problem.toString()+"toggle()");
+		
+		labela.setAttribute("class", "btn btn-primary");
+		
+		labela.setAttribute("type", "button");
+		
+		labela.setAttribute("data-toggle", "collapse");
+		
+		labela.setAttribute("href", "#map");
+		
+		var problemDescriptionLabel = document.createTextNode(problem.toString());
+		
+		labela.appendChild(problemDescriptionLabel);
+		
+		var descriptDiv = document.createElement("div");
+		descriptDiv.setAttribute("class", "collapse");
+		descriptDiv.setAttribute("id", problem.toString());
+		
+		var descriptionString = document.createTextNode(description.toString());
+		
+		container.appendChild(row);
+		row.appendChild(colm);
+		colm.appendChild(labela);
+		colm.appendChild(descriptDiv);
+		descriptDiv.appendChild(descriptionString);
+		colm.appendChild(scripts);
+	}
 	
-	labela.setAttribute("class", "btn btn-primary");
-	
-	labela.setAttribute("type", "button");
-	
-	labela.setAttribute("data-toggle", "collapse");
-	
-	labela.setAttribute("href", "#map");
-	
-	
-	
-	var descriptDiv = document.createElement("div");
-	descriptDiv.setAttribute("class", "collapse");
-	descriptDiv.setAttribute("id", problem.toString());
-	
-	var descriptionString = document.createTextNode(description.toString());
-	
-	var scripts = document.createElement("script");
-	scripts.text = "function " +problem.toString()+"toggle (){" +"$('#" + problem.toString() +").collapse('toggle');}";
-	
-	colm.appendChild(scripts);
-	descriptDiv.appendChild(descriptionString);
-	colm.appendChild(descriptDiv);
-	colm.appendChild(labela);
-	row.appendChild(colm);
-	container.appendChild(row);
-	
+	document.getElementById("test").appendChild(container);
 	///////////////////////
-	document.body.append(container);
+//	document.body.append(container);
 
 }
