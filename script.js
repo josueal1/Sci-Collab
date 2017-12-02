7fdd822356f904a6dea8edc2c226f8971afd0b6b
D Kim <d.dkim026@gmail.com>
2017-12-02 06:04:06 -0500

    aaaaa
    aaaa

---
+++ script.js
@@ -0,0 +1,51 @@
+function generate ()
+{
+	
+	var container = document.createElement("div");
+	container.setAttribute("class", "container"); //Have to append each of the following 
+
+/*	var problem = fields[k].problem;
+	var description = fields[k].description;*/
+
+	var row = document.createElement("div");
+	row.setAttribute("class", "row");
+	
+	var colm = document.createElement("div");
+	colm.setAttribute("class", "col-sm");
+	
+	
+	var labela = document.createElement("a");
+	labela.setAttribute("onmouseover", problem.toString()+"()");
+
+	labela.setAttribute("onmouseout", problem.toString()+"()");
+	
+	labela.setAttribute("class", "btn btn-primary");
+	
+	labela.setAttribute("type", "button");
+	
+	labela.setAttribute("data-toggle", "collapse");
+	
+	labela.setAttribute("href", "#map");
+	
+	
+	
+	var descriptDiv = document.createElement("div");
+	descriptDiv.setAttribute("class", "collapse");
+	descriptDiv.setAttribute("id", problem.toString());
+	
+	var descriptionString = document.createTextNode(description.toString());
+	
+	var scripts = document.createElement("script");
+	scripts.text = "function (){" +"$('#" + problem.toString() +").collapse('toggle');}";
+	
+	colm.appendChild(scripts);
+	descriptDiv.appendChild(descriptionString);
+	colm.appendChild(descriptDiv);
+	colm.appendChild(labela);
+	row.appendChild(colm);
+	container.appendChild(row);
+	
+	///////////////////////
+	document.body.append(container);
+
+}
