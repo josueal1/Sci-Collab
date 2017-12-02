59647db64c2e08572ea7355fa0dbb3c14de7aa15
Anarchira <zhuyl123456@126.com>
2017-12-02 07:51:47 -0500

    combined

--- Database Interaction/index.js
+++ Database Interaction/index.js
@@ -5,10 +5,13 @@ var submits=document.getElementById("submit");
 
 function submitting(){
 var firebaseRef = firebase.database().ref(discipline.value);
+var count=Math.random();
 var data={
 problem: problemName.value,
-description: mainText.value 
+description: mainText.value,
+counter: count
 }
+
 firebaseRef.push(data);
 }
 
@@ -19,15 +22,67 @@ firebaseRef.on('value',gotData,errData);
 }
 
 function gotData(data){
-document.getElementById("listproblem").innerHTML="";
+//document.getElementById("listproblem").innerHTML="";
 console.log(data.val());
 var fields =data.val();
 var keys=Object.keys(fields);
+document.getElementById("retrieveddata").innerHTML="";
 for (var i=0; i<keys.length; i++){
 var k=keys[i];
 var problem=fields[k].problem;
 var description=fields[k].description;
 
+	
+	var container = document.createElement("div");
+	container.setAttribute("class", "container"); //Have to append each of the following 
+
+	var row = document.createElement("div");
+	row.setAttribute("class", "row");
+				
+		var scripts = document.createElement("script");
+		scripts.text = "function " +problem.toString()+"toggle (){" +"$('#" + problem.toString() +"').collapse('toggle');}";
+		document.head.appendChild(scripts);
+
+		
+		
+		var colm = document.createElement("div");
+		colm.setAttribute("class", "col-sm");
+		
+		
+		var labela = document.createElement("a");
+		labela.setAttribute("onmouseover", problem.toString()+"toggle()");
+
+		labela.setAttribute("onmouseout", problem.toString()+"toggle()");
+		
+		labela.setAttribute("class", "btn btn-primary");
+		
+		labela.setAttribute("type", "button");
+		
+		labela.setAttribute("data-toggle", "collapse");
+		
+		labela.setAttribute("href", "#map");
+		
+		var problemDescriptionLabel = document.createTextNode(problem.toString());
+		
+		labela.appendChild(problemDescriptionLabel);
+		
+		var descriptDiv = document.createElement("div");
+		descriptDiv.setAttribute("class", "collapse");
+		descriptDiv.setAttribute("id", problem.toString());
+		
+		var descriptionString = document.createTextNode(description.toString());
+		
+		container.appendChild(row);
+		row.appendChild(colm);
+		colm.appendChild(labela);
+		colm.appendChild(descriptDiv);
+		descriptDiv.appendChild(descriptionString);
+		colm.appendChild(scripts);
+
+	
+	document.getElementById("retrieveddata").appendChild(container);
+	///////////////////////
+//	document.body.append(container);
 //window.alert(problem + ": " + description);
 //document.write(problem + ": " + description);
 //document.getElementById("listproblem").innerHTML=document.getElementById("listproblem").innerHTML + (i+1)+". "+problem + ": " + description + "<br>";
